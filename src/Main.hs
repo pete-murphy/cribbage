54ec3d83db94c3882b2c683a0df0ad1074beaf78
Peter Murphy <pete@simspace.com>
2021-08-04 17:20:50 -0400

    Add Shuffle.Sequence

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Criterion.Main
 import qualified Shuffle.List as List
+import qualified Shuffle.Sequence as Sequence
 import qualified Shuffle.Vector as Vector
 import System.Random
 
@@ -12,19 +13,25 @@ main =
         [ bgroup
             "List.shuffle"
             [ bench "10" $ whnf (List.shuffle stdGen) [0 .. 10],
-              bench "1000" $ whnf (List.shuffle stdGen) [0 .. 1000],
-              bench "10000" $ whnf (List.shuffle stdGen) [0 .. 10000]
+              bench "1000" $ whnf (List.shuffle stdGen) [0 .. 1000]
+              -- bench "10000" $ whnf (List.shuffle stdGen) [0 .. 10000]
             ],
           bgroup
             "Vector.shuffle"
             [ bench "10" $ whnf (Vector.shuffle stdGen) [0 .. 10],
-              bench "1000" $ whnf (Vector.shuffle stdGen) [0 .. 1000],
-              bench "10000" $ whnf (Vector.shuffle stdGen) [0 .. 10000]
+              bench "1000" $ whnf (Vector.shuffle stdGen) [0 .. 1000]
+              -- bench "10000" $ whnf (Vector.shuffle stdGen) [0 .. 10000]
             ],
           bgroup
-            "Vector.shuffle'"
-            [ bench "10" $ whnf (Vector.shuffle' stdGen) [0 .. 10],
-              bench "1000" $ whnf (Vector.shuffle' stdGen) [0 .. 1000],
-              bench "10000" $ whnf (Vector.shuffle' stdGen) [0 .. 10000]
+            "Sequence.shuffle"
+            [ bench "10" $ whnf (Sequence.shuffle stdGen) [0 .. 10],
+              bench "1000" $ whnf (Sequence.shuffle stdGen) [0 .. 1000]
+              -- bench "10000" $ whnf (Vector.shuffle' stdGen) [0 .. 10000]
             ]
+          -- bgroup
+          --   "Vector.shuffle'"
+          --   [ bench "10" $ whnf (Vector.shuffle' stdGen) [0 .. 10],
+          --     bench "1000" $ whnf (Vector.shuffle' stdGen) [0 .. 1000],
+          --     -- bench "10000" $ whnf (Vector.shuffle' stdGen) [0 .. 10000]
+          --   ]
         ]
--- src/Shuffle/Sequence.hs
+++ src/Shuffle/Sequence.hs
@@ -9,18 +9,22 @@ import qualified Control.Monad.IO.Class as IO.Class
 import qualified Control.Monad.State as State
 import Data.Foldable
 import qualified Data.Sequence as Sequence
-import Data.Sequence (Seq)
+import Data.Sequence ((><), Seq, ViewL (..))
 import Shuffle.Random as Random
 import System.Random (RandomGen, StdGen)
 import qualified System.Random
 
 shuffle :: RandomGen g => g -> [a] -> [a]
-shuffle gen (Sequence.fromList -> s) = toList (go gen Sequence.empty s)
+shuffle gen (Sequence.fromList -> s) = go gen [] s (length s)
   where
-    go :: RandomGen g => g -> Seq a -> Seq a -> Seq a
-    go = undefined
+    -- go :: RandomGen g => g -> Seq a -> Seq a -> Seq a
+    go _ acc _ 0 = acc
+    go g acc s l =
+      let (n, g') = System.Random.next g
+          (leftS, (Sequence.viewl -> (pick :< rightS))) = Sequence.splitAt (n `mod` l) s
+       in go g' (pick : acc) (leftS >< rightS) (l - 1)
 -- go _ acc [] = acc
 -- go gen' acc xs =
---   let (n, gen'') = System.Random.next gen'
+--   let
 --       (xs', pick : xs'') = List.splitAt (n `mod` length xs) xs
 --    in go gen'' (pick : acc) (xs' <> xs'')
