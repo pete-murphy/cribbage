13277b35710f14ec8d437e59be256d0d4e9134e4
Peter Murphy <pete@simspace.com>
2021-12-19 15:02:38 -0500

    Add activePlayer, cutDeck, playCard

--- src/Cribbage/Game.hs
+++ src/Cribbage/Game.hs
@@ -1,12 +1,13 @@
 module Cribbage.Game where
 
-import Card (Card)
+import Card (Card, Rank (..))
 import qualified Card
 import qualified Control.Exception as Exception
-import Control.Lens.Combinators
+import Control.Lens.Combinators hiding (_Unwrapped)
 import Control.Lens.Operators
 import qualified Data.Foldable as Foldable
 import Data.Generics.Labels ()
+import Data.Generics.Wrapped (_Unwrapped)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import GHC.Generics (Generic)
@@ -21,11 +22,14 @@ data Player = A | B
 
 newtype Board
   = Board (Map Player Int)
+  deriving (Generic)
 
 type PlayerHands = Map Player [Card]
 
 type Crib = [Card]
 
+-- Player can be skipped, and we need to recover the player after cards have
+-- been played. Last played card will be at top of stack / head of list.
 type CardsInPlay =
   [(Player, Maybe Card)]
 
@@ -40,7 +44,7 @@ data PlayState
 
 data GameState
   = GameState
-      { dealer :: Player,
+      { dealer :: Maybe Player,
         deck :: [Card],
         board :: Board,
         play :: PlayState
@@ -51,23 +55,38 @@ initialState :: RandomGen g => g -> GameState
 initialState gen =
   GameState
     { -- TODO: Initial dealer could be determined by drawing lowest card
-      dealer = A,
+      dealer = Just A,
       deck = Shuffle.List.shuffle gen Card.deck,
       board = Board (Map.fromList [(A, 0), (B, 0)]),
       -- play = Initial (Map.fromList [(A, []), (B, [])])
       play = PlayState (Map.fromList [(A, []), (B, [])]) Nothing Nothing Nothing
     }
 
+-- | We can always derive the active player from current game state by looking
+-- at who played the last card (or if there are no cards played, its the
+-- dealer's turn)
+activePlayer :: GameState -> Player
+activePlayer gameState =
+  case gameState ^? failing (#play . #cardsInPlay . _Just . ix 0 . _1) (#dealer . _Just) of
+    Just player -> player
+    _ -> error "Failed to get active player"
+
+otherPlayer :: Player -> Player
+otherPlayer = \case
+  A -> B
+  B -> A
+
 deal ::
   Int -> -- How many to deal (could fix to 6)
   GameState ->
   GameState
-deal n gameState = case List.splitAtMany [n, n] (deck gameState) of
-  Just [a, b, rest] ->
-    gameState
-      & #play . #playerHands . partsOf traversed .~ [a, b]
-      & #deck .~ rest
-  _ -> error "Not enough cards to deal"
+deal n gameState =
+  case List.splitAtMany [n, n] (deck gameState) of
+    Just [a, b, rest] ->
+      gameState
+        & #play . #playerHands . partsOf traversed .~ [a, b]
+        & #deck .~ rest
+    _ -> error "Not enough cards to deal"
 
 makeCrib ::
   [Card] -> -- Which cards player A chooses to put in crib
@@ -80,10 +99,38 @@ makeCrib cribA cribB gameState =
       List.select cribB -> Just restB
       ] ->
         gameState
-          -- Just `#play . #playerHands . traversed` would be a
+          -- `#play . #playerHands . traversed` would be a
           -- `Traversal' GameState [Card]`
           -- `partsOf` makes that a `Lens' GameState [[Card]]`
           -- so we can update both hands at the same time
           & #play . #playerHands . partsOf traversed .~ [restA, restB]
           & #play . #crib ?~ cribA <> cribB
     _ -> error "Failed to make crib"
+
+cutDeck :: GameState -> GameState
+cutDeck gameState =
+  let dealer = activePlayer gameState
+   in case List.selectIxs [0] (deck gameState) of
+        Just ([cutCard], rest) ->
+          gameState
+            & #deck .~ rest
+            & #play . #cutCard ?~ cutCard
+            -- If cut card is Jack, dealer gets two points ("his heels")
+            & #board . _Unwrapped . ix dealer . filtered (\_ -> Card.rank cutCard == Jack) +~ 2
+        _ -> error "Failed to cut deck"
+
+-- TODO: Update score on card play
+playCard :: Card -> GameState -> GameState
+playCard card gameState =
+  let player = activePlayer gameState
+      sumOfCardsInPlay =
+        sumOf
+          (#play . #cardsInPlay . _Just . folded . _2 . _Just . to Card.value)
+          gameState
+      canPlayCard = sumOfCardsInPlay + Card.value card <= 31
+   in case (List.select [card] (gameState ^. #play . #playerHands . ix player), canPlayCard) of
+        (Just rest, True) ->
+          gameState
+            & #play . #playerHands . ix player .~ rest
+            & #play . #cardsInPlay . _Just %~ (:) (player, Just card)
+        _ -> error "Failed to play card"
